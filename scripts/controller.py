1ab82c22e50ff9829af5666c99d31fcaf24cb635
tsuchiya-i <41734571+tsuchiya-i@users.noreply.github.com>
2022-03-29 17:44:45 +0900

    forced stop added

--- scripts/controller.py
+++ scripts/controller.py
@@ -11,43 +11,58 @@ from geometry_msgs.msg import Twist
 import numpy as np
 import math
 
-pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
-def callback(data):
-    pub_vel =  publish_vel(data)
-    pub.publish(pub_vel)
+class joy_controller():
+    def __init__(self):
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.forced_stop = False
 
-def main():
-    rospy.init_node('controller', anonymous=True)
-    rospy.Subscriber("/joy" ,Joy ,callback)
-    rospy.spin()
+    def callback(self, data):
+        if sum(data.buttons[8:11]) > 0:
+            self.forced_stop = not self.forced_stop
+        self.publish_vel(data)
+    
+    def publish_vel(self, data):
+        pub_vel = Twist()
+        
+        Lstick_RL = data.axes[0]
+        Lstick_FB = data.axes[1]
+        pub_vel.linear.x = Lstick_FB
+        pub_vel.angular.z = Lstick_RL
+        
+        R2 = (-data.axes[5]+1.0)/2.0
+        L2 = (data.axes[2]-1.0)/2.0
+        
+        if data.buttons[7] == 1:
+            pub_vel.linear.x = R2
+        if data.buttons[6] == 1:
+            pub_vel.linear.x = L2
+        
+        if data.buttons[0] == 1:
+            pub_vel.linear.x = 0
+            pub_vel.angular.z = 0
+        if data.buttons[1] == 1:
+            pub_vel.linear.x = 1.0
+        if data.buttons[3] == 1:
+            pub_vel.linear.x = -1.0
+        self.pub.publish(pub_vel)
 
-def publish_vel(data):
-    pub_vel = Twist()
-
-    Lstick_RL = data.axes[0]
-    Lstick_FB = data.axes[1]
-    pub_vel.linear.x = Lstick_FB
-    pub_vel.angular.z = Lstick_RL
-
-
-    R2 = (-data.axes[5]+1.0)/2.0
-    L2 = (data.axes[2]-1.0)/2.0
-
-    if data.buttons[7] == 1:
-        pub_vel.linear.x = R2
-    if data.buttons[6] == 1:
-        pub_vel.linear.x = L2
-
-    if data.buttons[0] == 1:
-        pub_vel.linear.x = 0
+    def publish_zero(self):
+        pub_vel = Twist()
         pub_vel.angular.z = 0
-    if data.buttons[1] == 1:
-        pub_vel.linear.x = 1.0
-    if data.buttons[3] == 1:
-        pub_vel.linear.x = -1.0
+        pub_vel.linear.x = 0
+        self.pub.publish(pub_vel)
 
-    return pub_vel
+    def main(self):
+        rospy.init_node('controller', anonymous=True)
+        rospy.Subscriber("/joy" ,Joy ,self.callback)
+        r = rospy.Rate(20) # 10hz
+        while not rospy.is_shutdown():
+            if self.forced_stop:
+                self.publish_zero()
+            r.sleep()
+        rospy.spin()
 
 if __name__ == '__main__':
-    main()
+    joycon = joy_controller()
+    joycon.main()
